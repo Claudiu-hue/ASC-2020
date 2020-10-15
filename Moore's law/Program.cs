25c33963d2f0018465f635c5938ba729ab7d4ac9
Luca Ionut Claudiu <claudiuluca311@yahoo.com>
2020-10-15 16:51:07 +0300

    commit

--- Moore's law/Program.cs
+++ Moore's law/Program.cs
@@ -10,7 +10,17 @@ namespace Moore_s_law
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("hello");
+            int i, n, j;
+            i = 1;
+            j = 1;
+            n = Convert.ToInt32(Console.ReadLine()); //puterea de calcul introdusa
+            while(i<n) 
+            {
+                i = i * 2; //puterea de calcul pe care o dublez
+                j = j + 2; //timpul parcurs
+
+            }
+            Console.WriteLine(j);
         }
     }
 }
